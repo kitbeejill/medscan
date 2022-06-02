455ed5ccba0e4090cb7ec364f3dc5880db8df354
kitbeejill <88554337+kitbeejill@users.noreply.github.com>
2022-06-02 16:59:04 -0500

    Add files via upload

---
+++ pipeline_config.groovy
@@ -0,0 +1,3 @@
+componentName = "react-preference-view"
+repoUrl = "git@bitbucket.org:medlineis/ecom-ui-mf-react-preference-view.git"
+helmChartName = "react-preference-view" //get it from Chart.yaml inside helm folder
