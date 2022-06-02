32e9e0c3d3cdeea1409562ba02add5cc89b04378
kitbeejill <88554337+kitbeejill@users.noreply.github.com>
2022-06-02 17:02:56 -0500

    Delete pipeline_config.groovy

--- pipeline_config.groovy
+++
@@ -1,3 +0,0 @@
-componentName = "react-preference-view"
-repoUrl = "git@bitbucket.org:medlineis/ecom-ui-mf-react-preference-view.git"
-helmChartName = "react-preference-view" //get it from Chart.yaml inside helm folder
